2af25138950fd5d1c82de14d23a90b42070e8a38
Il Harper <afanyiyu@hotmail.com>
2020-07-13 19:54:50 +0800

    fix: LibASS Bugfix

--- LibAss/LibAssContext.cs
+++ LibAss/LibAssContext.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
