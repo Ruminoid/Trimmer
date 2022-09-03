dfb4fb4a069734ae093e7cc705049ba60bc95846
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-09-03 22:32:45 +0800

    倍速播放 播放按钮图标与文本随播放状态变化

--- Views/PlaybackViewDataContext.cs
+++ Views/PlaybackViewDataContext.cs
@@ -27,7 +27,22 @@ namespace Ruminoid.Trimmer.Views
                 OnPropertyChanged();
             }
         }
+        private double _playSpeed = 1;
 
+        public double PlaySpeed
+        {
+            get => _playSpeed;
+            set
+            {
+                if (value > 3) value = 3;
+                if (value < 0.1) value = 0.1;
+                _playSpeed = value;
+                OnPropertyChanged();
+                PlaySpeedChanged?.Invoke(_playSpeed);
+            }
+        }
+        public delegate void PlaySpeedChangedHandler(double speed);
+        public event PlaySpeedChangedHandler PlaySpeedChanged;
         public Position Position { get; } = new Position();
 
         private bool _playing;
@@ -40,15 +55,42 @@ namespace Ruminoid.Trimmer.Views
                 if (!MediaLoaded) return;
                 _playing = value;
                 if (value)
+                {
                     VideoElement.Play();
+                    PlayButtonIconType = "Pause";
+                    PlayButtonText = "暂停";
+                }
                 else
+                {
                     VideoElement.Pause();
+                    PlayButtonIconType = "Run";
+                    PlayButtonText = "播放";
+                }
                 if (value) ThemeService.Current.ChangeAccent(Accent.Orange);
                 else ThemeService.Current.ChangeAccent(Accent.Blue);
                 OnPropertyChanged();
             }
         }
-
+        public string _playButtonIconType = "Run";
+        public string PlayButtonIconType
+        {
+            get => _playButtonIconType;
+            set
+            {
+                _playButtonIconType = value;
+                OnPropertyChanged();
+            }
+        }
+        public string _playButtonText = "播放";
+        public string PlayButtonText
+        {
+            get => _playButtonText;
+            set
+            {
+                _playButtonText = value;
+                OnPropertyChanged();
+            }
+        }
         #endregion
 
         #region PropertyChanged
